3221e0ad68cae41aa0c8b3e5b4b6c197349800dc
이성진 <chlskatnr1@naver.com>
2020-10-18 19:39:36 +0900

    [ADD] Animate / The animate directive

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,8 @@
   import Lifecycle from "./sections/Lifecycle.svelte";
   import Stores from "./sections/Stores.svelte";
   import Motion from "./sections/Motion.svelte";
+  import Transitions from "./sections/Transitions.svelte";
+  import Animate from "./sections/Animate.svelte";
 
   import Box from "./Box.svelte";
   import Hoverable from "./Hoverable.svelte";
@@ -30,119 +32,6 @@
   import AudioPlayer, { stopAll } from "./AudioPlayer.svelte";
   import Debugging from "./Debugging.svelte";
   import Congratulations from "./Congratulations.svelte";
-  import Transitions from "./sections/Transitions.svelte";
-
-  const progress = tweened(0, {
-    duration: 400,
-    easing: cubicOut,
-  });
-
-  let springCoords = spring(
-    { x: 50, y: 50 },
-    {
-      stiffness: 0.1,
-      damping: 0.25,
-    }
-  );
-  let springSize = spring(10);
-
-  let visible = true;
-
-  function spin(node, { duration }) {
-    return {
-      duration,
-      css: (t) => {
-        const eased = elasticOut(t);
-        return `
-          transform: scale(${eased}) rotate(${eased * 1080}deg);
-          color: hsl(
-						${~~(t * 360)},
-						${Math.min(100, 1000 - 1000 * t)}%,
-						${Math.min(50, 500 - 500 * t)}%
-					);
-        `;
-      },
-    };
-  }
-
-  function typewriter(node, { speed = 50 }) {
-    const vaild =
-      node.childNodes.length === 1 &&
-      node.childNodes[0].nodeType === Node.TEXT_NODE;
-
-    if (!vaild) {
-      throw new Error(
-        "This transition only work on elements with a single text node child"
-      );
-    }
-
-    const text = node.textContent;
-    const duration = text.length * speed;
-
-    return {
-      duration,
-      tick: (t) => {
-        const i = ~~(text.length * t);
-        node.textContent = text.slice(0, i);
-      },
-    };
-  }
-
-  let status = "wating...";
-
-  let showItems = true;
-  let i = 5;
-  let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  const [send, receive] = crossfade({
-    duration: (d) => Math.sqrt(d * 200),
-
-    fallback(node, params) {
-      const style = getComputedStyle(node);
-      const transform = style.transform === "none" ? "" : style.transform;
-
-      return {
-        duration: 600,
-        easing: quintOut,
-        css: (t) => `
-					transform: ${transform} scale(${t});
-					opacity: ${t}
-				`,
-      };
-    },
-  });
-
-  let uid = 1;
-
-  let todos2 = [
-    { id: uid++, done: false, description: "write some docs" },
-    { id: uid++, done: false, description: "start writing blog post" },
-    { id: uid++, done: true, description: "buy some milk" },
-    { id: uid++, done: false, description: "mow the lawn" },
-    { id: uid++, done: false, description: "feed the turtle" },
-    { id: uid++, done: false, description: "fix some bugs" },
-  ];
-
-  function add2(input) {
-    const todo = {
-      id: uid++,
-      done: false,
-      description: input.value,
-    };
-
-    todos2 = [todo, ...todos2];
-    input.value = "";
-  }
-
-  function remove2(todo) {
-    todos2 = todos2.filter((t) => t !== todo);
-  }
-
-  function mark2(todo, done) {
-    todo.done = done;
-    remove2(todo);
-    todos2 = todos2.concat(todo);
-  }
 
   const coords2 = spring(
     { x: 0, y: 0 },
@@ -271,123 +160,9 @@
   <Stores />
   <Motion />
   <Transitions />
+  <Animate />
 
   <!-- 
-
-  <progress value={$progress} />
-  <button on:click={() => ($progress = 0)}>0%</button>
-  <button on:click={() => progress.set(0)}>0%</button>
-  <button on:click={() => progress.set(0.25)}>25%</button>
-  <button on:click={() => progress.set(0.5)}>50%</button>
-  <button on:click={() => progress.set(0.75)}>75%</button>
-  <button on:click={() => progress.set(1)}>100%</button>
-
-  <div class="spring-wrap">
-    <div style="position: absolute; right: 1em;">
-      <label>
-        <h3>stiffness ({springCoords.stiffness})</h3>
-        <input
-          bind:value={springCoords.stiffness}
-          type="range"
-          min="0"
-          max="1"
-          step="0.01" />
-      </label>
-
-      <label>
-        <h3>damping ({springCoords.damping})</h3>
-        <input
-          bind:value={springCoords.damping}
-          type="range"
-          min="0"
-          max="1"
-          step="0.01" />
-      </label>
-    </div>
-
-    <svg
-      on:mousemove={(e) => ($springCoords = { x: e.offsetX, y: e.offsetY })}
-      on:mousedown={() => ($springSize = 30)}
-      on:mouseup={() => ($springSize = 10)}>
-      <circle cx={$springCoords.x} cy={$springCoords.y} r={$springSize} />
-    </svg>
-  </div>
-
-  <label> <input type="checkbox" bind:checked={visible} /> visible </label>
-  <p>status: {status}</p>
-  {#if visible}
-    <p transition:fade={{ duration: 500, easing: cubicOut }}>
-      Fades in and out
-    </p>
-
-    <p transition:fly={{ y: -200, duration: 2000, easing: cubicOut }}>
-      Fades in and out
-    </p>
-
-    <p in:fly={{ y: -200, duration: 2000, easing: cubicOut }} out:fade>
-      Fades in and out
-    </p>
-
-    <div style="position: relative;">
-      <div class="centered" in:spin={{ duration: 8000 }} out:fade>
-        <span class="spin">transitions!</span>
-      </div>
-    </div>
-
-    <p in:typewriter>The quick brown fox jumps over the lazy dog</p>
-
-    <p
-      transition:fly={{ y: 200, duration: 2000 }}
-      on:introstart={() => (status = 'intro started')}
-      on:introend={() => (status = 'intro ended')}
-      on:outrostart={() => (status = 'outro started')}
-      on:outroend={() => (status = 'outro ended')}>
-      Flies in and out
-    </p>
-  {/if}
-
-  <label> <input type="checkbox" bind:checked={showItems} /> show list </label>
-  <label> <input type="range" bind:value={i} max="10" /> </label>
-
-  {#if showItems}
-    {#each items.slice(0, i) as item}
-      <div class="item" transition:slide|local>{item}</div>
-    {/each}
-  {/if}
-
-  <div class="borad">
-    <input
-      type="text"
-      placeholder="What needs to do done?"
-      on:keydown={(e) => e.key === 'Enter' && add2(e.target)} />
-    <div class="left">
-      <h2>todo</h2>
-      {#each todos2.filter((t) => !t.done) as todo (todo.id)}
-        <label
-          in:receive={{ key: todo.id }}
-          out:send={{ key: todo.id }}
-          animate:flip={{ duration: 500 }}>
-          <input type="checkbox" on:change={() => mark2(todo, true)} />
-          {todo.description}
-          <button on:click={() => remove2(todo)}>remove</button>
-        </label>
-      {/each}
-    </div>
-
-    <div class="right">
-      <h2>done</h2>
-      {#each todos2.filter((t) => t.done) as todo (todo.id)}
-        <label
-          in:receive={{ key: todo.id }}
-          out:send={{ key: todo.id }}
-          animate:flip={{ duration: 200 }}>
-          <input type="checkbox" checked on:change={() => mark2(todo, false)} />
-          {todo.description}
-          <button on:click={() => remove2(todo)}>remove</button>
-        </label>
-      {/each}
-    </div>
-  </div>
 
   <div style="position: relative;">
     <div
